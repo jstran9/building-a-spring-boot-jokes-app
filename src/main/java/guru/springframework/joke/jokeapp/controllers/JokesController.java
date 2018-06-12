3e274e42fa171e6ae78ea789d76d6d10c04dd7c3
todd <toddtran9@gmail.com>
2018-06-11 20:56:52 -0700

    Creating the controller.

---
+++ src/main/java/guru/springframework/joke/jokeapp/controllers/JokesController.java
@@ -0,0 +1,24 @@
+package guru.springframework.joke.jokeapp.controllers;
+
+import guru.springframework.joke.jokeapp.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+
+    private JokeService jokeService;
+
+    @Autowired
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String getJokes(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+        return "chucknorris";
+    }
+}

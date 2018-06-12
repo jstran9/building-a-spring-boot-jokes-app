2d099bf459a1c926e14afe59b9c5d2fa81b75f84
todd <toddtran9@gmail.com>
2018-06-11 21:22:03 -0700

    Noting the ascii art site used to create banner.txt

--- src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/jokeapp/JokeappApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JokeappApplication {
 
     public static void main(String[] args) {
+        // go to, http://patorjk.com/software/taag/#p=display&f=Avatar&t=Spring%0AFramework%0AGuru
+        // for generation of the banner.txt's contents.
         SpringApplication.run(JokeappApplication.class, args);
     }
 }
